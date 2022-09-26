6aa222be4073368ccff27540391d718facc13acc
Lucas Joannes <96151383+joannescode@users.noreply.github.com>
2022-09-26 16:30:01 -0300

    Rename eXcript - Calculadora 1.0 #4.c to calc1.0#4.c

--- eXcript - C/calc1.0#4.c
+++ eXcript - C/calc1.0#4.c
@@ -10,7 +10,7 @@ int main (){
     int soma, sub, multi, div;
     
     printf("Calculadora 1.0\n");
-    printf("Por favor digite os dois números a serem processados:\n");
+    printf("Por favor digite os dois nÃºmeros a serem processados:\n");
     scanf("%i %i", &num1, &num2);
 
     soma = num1 + num2;
@@ -18,10 +18,10 @@ int main (){
     multi = num1 * num2;
     div = num1 / num2;
 
-    printf("O resultado da soma é:%i\n", soma);
-    printf("O resultado da subtração é:%i\n", sub);
-    printf("O resultado da multiplicação é:%i\n", multi);
-    printf("O resultado da divisão é:%i\n\n", div);
+    printf("O resultado da soma Ã©:%i\n", soma);
+    printf("O resultado da subtraÃ§Ã£o Ã©:%i\n", sub);
+    printf("O resultado da multiplicaÃ§Ã£o Ã©:%i\n", multi);
+    printf("O resultado da divisÃ£o Ã©:%i\n\n", div);
 
     system("pause");
     return 0;
